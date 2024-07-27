f4db5a381fcacdff6a3c0ccb8a2cb2d5f83d3bfc
bilol <xusanboyevbilol@gmail.com>
2024-07-27 16:24:59 +0500

    sqlite

--- root/settings.py
+++ root/settings.py
@@ -70,7 +70,10 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # }
 
 DATABASES = {
-    'default': 'django.db.backends.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
